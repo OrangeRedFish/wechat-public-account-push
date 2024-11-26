40f4509abe1ea82816df63c9d2d4241ee5f9bc77
OrangeRedFish <112415574+OrangeRedFish@users.noreply.github.com>
2024-11-26 22:32:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCRPP6E7GiAo5EhL5XaQ6E7tfpOk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'X4oMuZGVDgbX-pTdAHx1QJYz23-3mXOoo-zSFQp1uCQ',
+      useTemplateId: '	453VuR6f3nTPi3aJro0jaqlZVtMmhjkF0vYyvrrKGwQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '7-11',
       festivals: [
